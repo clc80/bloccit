fd70746f23d36990c28d6e0938920e14625613b1
clc80 <clc_80@yahoo.com>
2018-08-24 20:06:29 -0700

    fixed running errors

--- src/server.js
+++ src/server.js
@@ -10,10 +10,10 @@ server.listen(port);
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
-  if(isNaN(port)) {
+  if (isNaN(port)) {
     return val;
   }
-  if(port >= 0) {
+  if (port >= 0) {
     return port;
   }
   return false;
