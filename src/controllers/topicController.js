30228917771a9dc67ce084b00f584c3c37e9bf92
clc80 <clc_80@yahoo.com>
2018-09-03 14:26:15 -0700

    finished Checkpoint #6

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -8,6 +8,64 @@ module.exports = {
       } else {
         res.render("topics/index", {topics});
       }
-    })
-  }
+    }) //end topicQueries.getAllTopics
+  }, //end index
+
+  new(req, res, next) {
+    res.render("topics/new");
+  }, // end new
+
+  create(req, res, next) {
+    let newTopic = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    topicQueries.addTopic(newTopic, (err, topic) => {
+      if(err) {
+        res.redirect(500, "/topics/new");
+      } else {
+        res.redirect(303, `/topics/${topic.id}`);
+      }
+    });//end topicQueries.addTopic
+  }, //end create
+
+  show(req, res, next) {
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if(err || topic == null) {
+        res.redirect(404, "/");
+      } else {
+        res.render("topics/show", {topic});
+      }
+    }); //end topicQueries.getTopic
+  }, //end show
+
+  destroy(req, res, next) {
+    topicQueries.deleteTopic(req.params.id, (err, topic) => {
+      if(err) {
+        res.redirect(500, `/topics/${topic.id}`)
+      } else {
+        res.redirect(303, "/topics")
+      }
+    }); //end topicQueries.deleteTopic
+  }, // end destroy
+
+  edit(req, res, next) {
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if(err || topic == null) {
+        res.redirect(404, "/");
+      } else {
+        res.render("topics/edit", {topic});
+      }
+    }); //end topicQueries.getTopic
+  },  //end edit
+
+  update(req, res, next) {
+    topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
+      if(err || topic == null) {
+        res.redirect(404, `/topics/$(req.params.id)/edit`);
+      } else {
+        res.redirect(`/topics/${topic.id}`);
+      }
+    }); //end topicQueries.updateTopic
+  } //end update
 }
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -10,5 +10,58 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
-  }
+  }, //end getAllTopics(callback)
+
+  getTopic(id, callback) {
+    return Topic.findById(id)
+    .then((topic) => {
+      callback(null,topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }, // end getTopic(id, callback)
+
+  addTopic(newTopic, callback){
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }, // end addTopic(newTopic, callback);
+
+  deleteTopic(id, callback){
+    return Topic.destroy({
+      where: {id}
+    }) //end return
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },  // end deleteTopic
+
+  updateTopic(id, updatedTopic, callback) {
+    return Topic.findById(id)
+    .then((topic) => {
+      if(!topic) {
+        return callback("Topic not found");
+      }
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
+  } //updateTopic
 }
