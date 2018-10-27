24c8a113f357771c3eacf37af4c95964db7210cd
clc80 <clc_80@yahoo.com>
2018-10-27 15:55:49 -0700

    Validations checkpoint almost done

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -77,6 +77,32 @@ describe("routes : posts", () => {
       ); //end request.post
     }); //end it
 
+    it("should not create a new post that fails validations", (done) => {/* spec implementation */});
+
+     it("should not create a new post that fails validations", (done) => {
+       const options = {
+         url: `${base}/${this.topic.id}/posts/create`,
+         form: {
+           title: "a",
+           body: "b"
+         }
+       };
+
+       request.post(options,
+         (err, res, body) => {
+           Post.findOne({where: {title: "a"}})
+           .then((post) => {
+               expect(post).toBeNull();
+               done();
+           })
+           .catch((err) => {
+             console.log(err);
+             done();
+           });
+         }
+       );
+     });
+
  }); //end describe("POST /topics/:topicId/posts/create")
 
  describe("GET /topics/:topicId/posts/:id", () => {
