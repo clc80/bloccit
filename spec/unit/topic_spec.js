8e56c5596e41944b010b75b6319686cb84bdc2e3
clc80 <clc_80@yahoo.com>
2018-11-02 16:28:36 -0700

    authorization checkpoint almost complete

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,34 +1,43 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
-
+const User = require("../../src/db/models").User;
 
 
 describe("Topic", () => {
   beforeEach((done) => {
     this.topic;
     this.post;
+    this.user;
+
     sequelize.sync({force: true}).then((res) => {
-      Topic.create({
-        title: "Expeditions to Alpha Centauri",
-        description: "A compilation of reports from recent visits to the star system."
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      })
+      .then((user) => {
+        this.user = user; //store the user
+
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+          include: {
+            model: Post,
+            as: "posts"
+          }
       })
       .then((topic) => {
         this.topic = topic;
-        Post.create({
-          title: "This is a post",
-          body:  "This is the body",
-          topicId: this.topic.id
-        })
-        .then((post) => {
-          this.post = post;
-          done();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
+        this.post = topic.posts[0];
         done();
-      });
+      })
+    })
     }); //end sequelize.sync
   }); //end before beforeEach
 
--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -8,7 +8,8 @@ module.exports = {
     let newPost= {
       title: req.body.title,
       body: req.body.body,
-      topicId: req.params.topicId
+      topicId: req.params.topicId,
+      userId: req.params.userId
     };
     postQueries.addPost(newPost, (err, post) => {
       if(err){
